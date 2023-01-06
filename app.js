ef4df31ef9f33ebba20a48c7c4708e4342c17f4a
JaronChow <jchow826@gmail.com>
2023-01-05 20:12:21 -0800

    "created wnews"

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require ("morgan")
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("hi"));
+app.use(morgan('dev'));
+
+
 
 const PORT = 1337;
-
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`App listening in port ${PORT} running on http://localhost:1337`);
 });
